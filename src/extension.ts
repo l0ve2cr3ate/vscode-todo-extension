82f971283c600613371730d64fb3b560c43b6887
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-01-10 11:00:16 +0100

    feat: Create webview.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,9 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
+
   context.subscriptions.push(
     vscode.commands.registerCommand("vstodo.helloWorld", () => {
-      vscode.window.showInformationMessage("Hello from VSTodo!");
+      HelloWorldPanel.createOrShow(context.extensionUri)
     })
   );
 
